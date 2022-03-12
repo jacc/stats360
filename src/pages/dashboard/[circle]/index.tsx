ce277d3055af919b7ec18c9b349a136e0a7bca97
Jack LaFond <hi@jack.link>
2022-03-12 15:47:50 -0500

    AA

--- src/pages/dashboard/[circle]/index.tsx
+++ src/pages/dashboard/[circle]/index.tsx
@@ -1,10 +1,59 @@
 import {useRouter} from 'next/router';
 import {useCircle} from '../../../client/hooks/circles/[id]';
+import circles from '../../api/users/@me/circles';
 
 export default function CirclePage() {
 	const router = useRouter();
 
 	const {data: circle} = useCircle((router.query.circle as string) ?? null);
 
-	return <pre>{JSON.stringify(circle, null, 2)}</pre>;
+	return (
+		<main className="mx-auto max-w-3xl py-24 space-y-6">
+			<div className="rounded-xl bg-white dark:bg-gray-800 p-12 space-y-6 shadow-sm dark:shadow-neutral-800/25 font-light border border-gray-300 dark:border-gray-700">
+				<div className="flex items-center space-x-3">
+					<h1 className=" font-medium text-2xl">
+						{circle?.name}'s Life360 Stats
+					</h1>
+				</div>
+				<div className="grid grid-cols-1 auto-cols-max md:grid-cols-2 gap-3">
+					<div>
+						<div>Members</div>
+						<div className="rounded-xl bg-white dark:bg-gray-800 p-4 space-y-6 shadow-sm dark:shadow-neutral-800/25 font-light border border-gray-300 dark:border-gray-700">
+							{circle
+								? circle.members.map(member => {
+										return <li>{member.firstName}</li>;
+								  })
+								: 'Loading...'}
+						</div>
+					</div>
+				</div>
+
+				{/* <div className="grid grid-cols-1 auto-cols-max md:grid-cols-2 gap-3">
+				{circles ? (
+					circles.length > 0 ? (
+						circles.map(circle => (
+							<Link key={circle.id} href={`/dashboard/${circle.id}`}>
+								<a className="hover:bg-pink-500/5 hover:text-pink-500 hover:border-pink-500/25 dark:hover:bg-pink-900/10 transition-all duration-150 group w-full flex justify-between border px-3 py-3 rounded-lg dark:bg-gray-900/50 dark:border-gray-800">
+									<p>{circle.name}</p>
+
+									<p className="text-gray-600 dark:text-gray-400 group-hover:text-pink-400">
+										<MdPeopleAlt className="inline -mt-1" />
+										&nbsp;
+										<span>{circle.memberCount}</span>
+									</p>
+								</a>
+							</Link>
+						))
+					) : (
+						<p>No circles!</p>
+					)
+				) : (
+					<p>Loading...</p>
+				)}
+			</div> */}
+			</div>
+		</main>
+	);
+
+	// return <pre>{JSON.stringify(circle, null, 2)}</pre>;
 }
