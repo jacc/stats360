b1c5e94adf5ea710fb6999c800a5b84eacd243a3
Jack LaFond <hi@jack.link>
2022-03-12 12:32:00 -0500

    !?!??!??!!?!?

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,15 @@ import {SWRConfig} from 'swr';
 import {fetcher} from '../client/fetcher';
 
 import 'tailwindcss/tailwind.css';
+import '../styles/index.css';
+import '@fontsource/plus-jakarta-sans';
 
 export default function App({Component, pageProps}: AppProps) {
 	return (
 		<SWRConfig value={{fetcher}}>
-			<Component {...pageProps} />
+			<div className="p-6">
+				<Component {...pageProps} />
+			</div>
 		</SWRConfig>
 	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,22 @@
+import 'tailwindcss/tailwind.css';
+
 export default function Home() {
-	return <div>a</div>;
+	return (
+		<main className="mx-auto max-w-3xl py-24 space-y-6">
+			<div className="rounded-lg bg-white p-6 shadow-sm shadow-cream-800/25 dark:shadow-neutral-800/25 font-light border border-gray-300 border-1">
+				<div className="font-bold text-2xl text-black">Welcome to Stats360</div>
+				<div className="font-light text-lg text-neutral-700">
+					Stats360 is a website to view Life360 Circle statistics. Please login
+					below.
+				</div>
+				<br></br>
+				<div>
+					<input
+						className="rounded-lg bg-white p-6 border border-neutral-200 border-2 text-black"
+						defaultValue="Login"
+					/>
+				</div>
+			</div>
+		</main>
+	);
 }
