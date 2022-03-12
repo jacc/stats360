2fdd0cb1b3248d3e0c11fada57b1d21a90184c2e
Alistair Smith <hi@alistair.sh>
2022-03-12 17:44:32 +0000

    ok

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,26 @@
+import Image from 'next/image';
+import Life360Logo from '../client/assets/life360-logo.png';
+
 export default function Home() {
 	return (
 		<main className="mx-auto max-w-3xl py-24 space-y-6">
-			<div className="rounded-lg bg-white p-6 shadow-sm shadow-cream-800/25 dark:shadow-neutral-800/25 font-light border border-gray-300 border-1">
-				<div className="font-bold text-2xl text-black">Welcome to Stats360</div>
-				<div className="font-light text-lg text-neutral-700">
-					Stats360 is a website to view Life360 Circle statistics. Please login
-					below.
+			<div className="rounded-lg bg-white p-12 space-y-6 shadow-sm shadow-cream-800/25 dark:shadow-neutral-800/25 font-light border border-gray-300 border-1">
+				<div className="flex items-center space-x-3">
+					<Image src={Life360Logo} height={48} width={48} alt="Life 360 Logo" />
+
+					<h1 className="font-bold text-2xl text-black">Welcome to Stats360</h1>
 				</div>
 
+				<h2 className="font-light text-lg text-neutral-500">
+					Stats360 is a website to view Life360 Circle statistics. Please login
+					below.
+				</h2>
+
 				<div>
-					<input defaultValue="Login" />
+					<input
+						className="border px-3 py-1.5 rounded-sm border-gray-800"
+						defaultValue="Login"
+					/>
 				</div>
 			</div>
 		</main>
