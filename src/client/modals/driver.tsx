cf4f7f1d1397bc74f53e63238b8e7f33bcfb7f7a
Jack LaFond <hi@jack.link>
2022-03-15 21:19:43 -0400

    new algo stats model

--- src/client/modals/driver.tsx
+++ src/client/modals/driver.tsx
@@ -1,5 +1,7 @@
 import {createModal} from './create';
 
+import {AiFillWarning} from 'react-icons/ai';
+
 import type API from '../../pages/api/circles/[id]/driving';
 import {InferAPIResponse} from 'nextkit';
 import {calculateScoreFor, Weighting} from '../../util/driver-score';
@@ -24,11 +26,11 @@ function result(arr: number[], key: Weighting) {
 	const avg = average(arr);
 	return (
 		<>
-			{avg}
-			<span className="text-gray-500">/</span>
-			{highest(arr)}
-			<span className="text-gray-500">/</span>
-			{fix(calculateScoreFor(key, avg))}
+			Averaging <span className="text-gray-500">{avg} </span> per trip,
+			<span className="text-gray-500">
+				-{fix(calculateScoreFor(key, avg))}
+			</span>{' '}
+			points
 		</>
 	);
 }
@@ -41,6 +43,15 @@ export const DriverModal = createModal<{trips: Option}>(props => {
 		content: (
 			<div className="mt-4">
 				<div className="grid grid-cols-1 gap-4">
+					<a
+						href="https://twitter.com/alistaiiiir"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="inline-block bg-yellow-500/25 text-yellow-500 px-3 py-1.5 rounded-full text-center"
+					>
+						<AiFillWarning className="inline-block" />
+						&nbsp; The algorithm is in beta and may not be accurate!
+					</a>
 					<div>
 						<div className="rounded-xl bg-white dark:bg-gray-800 p-4 shadow-sm dark:shadow-neutral-800/25 font-light border border-gray-300 dark:border-gray-700">
 							<div className="flex-shrink-0 flex items-center space-x-1 ">
@@ -55,18 +66,74 @@ export const DriverModal = createModal<{trips: Option}>(props => {
 						<div className="rounded-xl bg-white dark:bg-gray-800 p-4 shadow-sm dark:shadow-neutral-800/25 font-light border border-gray-300 dark:border-gray-700">
 							<div className="flex-shrink-0 flex items-center space-x-1 ">
 								<h1 className="font-semibold text-sm sm:text-regular">
-									Times distracted while driving
+									Times distracted during drives
 								</h1>
 							</div>
 							<p className="text-xs">
-								-
-								{fix(
-									calculateScoreFor(
-										'distractedCount',
-										average(trips.map(trip => trip.distractedCount)),
-									),
-								)}{' '}
-								points
+								{result(
+									trips.map(trip => trip.distractedCount),
+									'distractedCount',
+								)}
+							</p>
+						</div>
+					</div>
+					<div>
+						<div className="rounded-xl bg-white dark:bg-gray-800 p-4 shadow-sm dark:shadow-neutral-800/25 font-light border border-gray-300 dark:border-gray-700">
+							<div className="flex-shrink-0 flex items-center space-x-1 ">
+								<h1 className="font-semibold text-sm sm:text-regular">
+									Times hard breaking during drives
+								</h1>
+							</div>
+							<p className="text-xs">
+								{result(
+									trips.map(trip => trip.hardBrakingCount),
+									'distractedCount',
+								)}
+							</p>
+						</div>
+					</div>
+					<div>
+						<div className="rounded-xl bg-white dark:bg-gray-800 p-4 shadow-sm dark:shadow-neutral-800/25 font-light border border-gray-300 dark:border-gray-700">
+							<div className="flex-shrink-0 flex items-center space-x-1 ">
+								<h1 className="font-semibold text-sm sm:text-regular">
+									Times speeding during drives
+								</h1>
+							</div>
+							<p className="text-xs">
+								{result(
+									trips.map(trip => trip.speedingCount),
+									'speedingCount',
+								)}
+							</p>
+						</div>
+					</div>
+					<div>
+						<div className="rounded-xl bg-white dark:bg-gray-800 p-4 shadow-sm dark:shadow-neutral-800/25 font-light border border-gray-300 dark:border-gray-700">
+							<div className="flex-shrink-0 flex items-center space-x-1 ">
+								<h1 className="font-semibold text-sm sm:text-regular">
+									Times rapidly accelerated during drives
+								</h1>
+							</div>
+							<p className="text-xs">
+								{result(
+									trips.map(trip => trip.rapidAccelerationCount),
+									'rapidAccelerationCount',
+								)}
+							</p>
+						</div>
+					</div>
+					<div>
+						<div className="rounded-xl bg-white dark:bg-gray-800 p-4 shadow-sm dark:shadow-neutral-800/25 font-light border border-gray-300 dark:border-gray-700">
+							<div className="flex-shrink-0 flex items-center space-x-1 ">
+								<h1 className="font-semibold text-sm sm:text-regular">
+									Average drive time
+								</h1>
+							</div>
+							<p className="text-xs">
+								<span className="text-gray-500">
+									~{Math.floor(average(trips.map(trip => trip.duration)) / 60)}
+								</span>{' '}
+								minutes
 							</p>
 						</div>
 					</div>
